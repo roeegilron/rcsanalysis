a4ee243e7773249654fdd43f79f2bf100d43b762
S-Little <42390748+S-Little@users.noreply.github.com>
2019-08-13 14:20:06 -0700

    fixed issue with cell arrays and sessions table

--- matlab/MAIN_report_data_in_folder.m
+++ matlab/MAIN_report_data_in_folder.m
@@ -6,13 +6,29 @@ else
 end
 
 tblout = getDataBaseRCSdata(dirname);
-% print out details about this to a text file in this directory 
+% print out details about this to a text file in this directory
 fid = fopen(fullfile(dirname,'recordingReport.txt'),'w+');
 
 for t = 1:size(tblout,1)
-    if ~isempty(tblout.startTime(t))        
+    if ~isempty(tblout.startTime(t))
+        % need to check if the table is comprised of cell arrays
+        % in some cases empty sessions will force the table into a cell
+        % array
+        % in other cases if all sessions are valid it will format a a
+        % matrix
+        if iscell(tblout.duration)
+            dur = tblout.duration{t};
+            startTime = tblout.startTime{t};
+            endTime = tblout.endTime{t};
+            sessName = tblout.sessname{t};
+        else
+            dur = tblout.duration(t);
+            startTime = tblout.startTime(t);
+            endTime = tblout.endTime(t);
+            sessName = tblout.sessname{t};
+        end
         fprintf(fid,'%s (Duration)\n\t\t%s\t\t%s\t\t%s\n',...
-            tblout.duration{t},tblout.startTime{t},tblout.endTime{t},tblout.sessname{t});
+            dur,startTime,endTime,sessName);
         
         et = tblout.eventData{t};
         if ~isempty(et)
@@ -29,23 +45,28 @@ for t = 1:size(tblout,1)
     end
 end
 
-toSum = {tblout.duration(~cellfun(@(x) isempty(x),tblout.duration))};
-totalDuration = sum([toSum{1,1}{:}]);
+
+if iscell(tblout.duration)
+    toSum = {tblout.duration(~cellfun(@(x) isempty(x),tblout.duration))};
+    totalDuration = sum([toSum{1,1}{:}]);
+else
+    totalDuration = sum(tblout.duration);
+end
 fnmsave = fullfile(dirname,'database.mat');
-save(fnmsave,'tblout','totalDuration'); 
+save(fnmsave,'tblout','totalDuration');
 return;
 
 idxuse = strcmp(et.EventType,'INSLeftBatteryLevel') & ...
-    ~isempty(et.EventSubType) & ... 
+    ~isempty(et.EventSubType) & ...
     ~strcmp(et.EventSubType,'%');
-etuse = et(idxuse,:); 
-percents = cellfun(@(x) str2num( strrep(x,'%','') ),etuse.EventSubType); 
-times = etuse.UnixOnsetTime; 
+etuse = et(idxuse,:);
+percents = cellfun(@(x) str2num( strrep(x,'%','') ),etuse.EventSubType);
+times = etuse.UnixOnsetTime;
 figure;
-plot(times,percents); 
-title('INS battery decline'); 
-xlabel('Time'); 
-ylabel('INS %'); 
-set(gca,'FontSize',16); 
+plot(times,percents);
+title('INS battery decline');
+xlabel('Time');
+ylabel('INS %');
+set(gca,'FontSize',16);
 
 end
