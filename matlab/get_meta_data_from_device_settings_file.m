ae5925eba1bc0070f5bb25738f88c20d16316161
Roee Gilron <roee@RoeeG.local.ucsf.edu>
2020-07-21 07:03:54 -0700

    creating a device settings meta data extractor

---
+++ matlab/get_meta_data_from_device_settings_file.m
@@ -0,0 +1,7 @@
+function metaData = get_meta_data_from_device_settings_file(fn)
+% this functions gets some meta data from a device settings file 
+% it depends on having other files that it will also attempt to open in the
+% same directory as the device settings file 
+
+
+end
